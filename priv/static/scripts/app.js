c27d2c725fecfcc94e0f42bc001c8705cd526b88
Jake Bray <jake.d.bray@gmail.com>
2018-02-25 22:10:07 -0500

    that's a good trick

--- priv/static/scripts/app.js
+++ priv/static/scripts/app.js
@@ -1,45 +1,87 @@
-(function IIFE() {
-  // Initialization
-  const SCREEN_NAME = "galactica"
-  openSite(`${SCREEN_NAME}.html`)
+// (function IIFE() {
 
-  // Selectors
+// Selectors
+const currentSite = document.querySelector("#currentSite")
+const upSite = document.querySelector("#upSite")
+const leftSite = document.querySelector("#leftSite")
+const rightSite = document.querySelector("#rightSite")
+const downSite = document.querySelector("#downSite")
 
-
-  // Logic
-  function fetchSites(site) {
-    if (site === '') {
-      return
+// Siteroom stuff
+const DIRS = ['left', 'right', 'up', 'down']
+function oppositeDirection(dir) {
+  switch (dir) {
+    case 'left':
+      return 'right';
+    case 'right':
+      return 'left';
+    case 'up':
+      return 'down';
+    case 'down':
+      return 'up';
+  }
+}
+class Site {
+  constructor(name, { prev, dir } = {}) {
+    this.name = name
+    if (dir) {
+      this[dir] = prev
     }
-    submit.setAttribute("disabled", true)
-    submit.textContent = "Thinking..."
-    fetch(`/lookup?site=${encodeURIComponent(site)}`).then(response => {
-      return response.json()
-    }).then(data => {
-      openSite(`http://${site}`, SCREEN_NAME)
-      const {
-        relatedSites
-      } = data
-      for (const site of relatedSites) {
-        if (!sites.includes(site)) {
-          sites.push(site)
-        }
-        sites.sort()
-        createSitelist()
+    this.setRelatedSites()
+  }
+  setRelatedSites() {
+    fetchSites(this.name).then(sites => {
+      sites = sites.filter(s => Site.addSite(s))
+      for (const dir of DIRS) {
+        this[dir] = this[dir] || sites.pop()
       }
-    }).catch((err => {
-      console.error(err)
-    })).then(() => {
-      resetInput()
     })
   }
-
-  function openSite(url) {
-    const windowRef = window.open(url, SCREEN_NAME, "menubar=0")
-    if (windowRef) {
-      return windowRef
-    } else {
-      alert("Are you alive? Please allow popups for this site.")
+  move(dir) {
+    let nextCurrent = this[dir]
+    if (!(nextCurrent instanceof Site)) {
+      // make nextCurrent is a proper Site object
+      nextCurrent = new Site(nextCurrent, { prev: this, dir: oppositeDirection(dir) })
+      // update current site's neighbor reference to hold proper Site object
+      this[dir] = nextCurrent
     }
+    return nextCurrent
   }
-})()
+  static addSite(name) {
+    if (Site.sites.has(name)) {
+      return false
+    }
+    Site.sites.add(name)
+    return true
+  }
+}
+Site.sites = new Set()
+
+// Initialization
+const SCREEN_NAME = "galactica"
+const siteName = location.hash && location.hash.slice(1) || "google.com"
+Site.addSite(siteName)
+let currentSite = new Site(siteName)
+openSite(`http://${siteName}`)
+
+// Logic
+function fetchSites(site) {
+  if (site === '') {
+    return
+  }
+  return fetch(`/lookup?site=${encodeURIComponent(site)}`).then(response => {
+    return response.json()
+  }).then(({ relatedSites }) => {
+    return relatedSites;
+  })
+}
+
+function openSite(url) {
+  const windowRef = window.open(url, SCREEN_NAME, "menubar=0")
+  if (windowRef) {
+    return windowRef
+  } else {
+    alert("Are you alive? Please allow popups for this site.")
+  }
+}
+// })()
