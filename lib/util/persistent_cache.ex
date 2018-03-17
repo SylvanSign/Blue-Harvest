508cc8b8cb93fa010eb96a84dc21f6bd9c401113
Jake Bray <jake.d.bray@gmail.com>
2018-03-17 19:05:54 -0400

    use ETS for caching

--- lib/util/persistent_cache.ex
+++ lib/util/persistent_cache.ex
@@ -1,8 +1,4 @@
 defmodule Util.PersistentCache do
-  def new(name) when is_atom(name) do
-    ^name = :ets.new(name, [:named_table])
-  end
-
   def get(table, key) when is_atom(table) do
     :ets.lookup(table, key)
     |> handle_lookup()
@@ -13,15 +9,37 @@ defmodule Util.PersistentCache do
     save(table)
   end
 
-  defp save(table) when is_atom(table) do
-    file_path = Util.Priv.get_priv_path(table)
+  # put all entries, where entries looks like [{key1, ...}, {key2, ...}, ...]
+  def put_many(table, entries) when is_list(entries) do
+    :ets.insert(table, entries)
+    save(table)
+  end
+
+  defp save(table) do
+    file_path = Util.Priv.get_priv_path("#{table}.state")
     # Note: :ets.tab2file/2 writes the file asynchronously
     :ets.tab2file(table, String.to_atom(file_path))
   end
 
-  def load(file_path) do
-    full_file_path = Util.Priv.get_priv_path(file_path)
-    :ets.file2tab(String.to_atom(full_file_path))
+  def load(table) do
+    file_path = Util.Priv.get_priv_path("#{table}.state")
+
+    if File.exists?(file_path) do
+      :ets.file2tab(String.to_atom(file_path))
+    else
+      {:ok, new(table)}
+    end
+  end
+
+  defp new(name) when is_atom(name) do
+    opts = [
+      :named_table,
+      :public,
+      read_concurrency: true,
+      write_concurrency: true
+    ]
+
+    ^name = :ets.new(name, opts)
   end
 
   defp handle_lookup([{_key, value}]), do: value
